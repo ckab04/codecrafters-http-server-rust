e9f1c4136a393816b43a9cfdd8973ca6e493b5e8
ckab04 <ciceronkapenabm@gmail.com>
2023-12-30 19:51:28 +0100

    Respond with content

--- src/main.rs
+++ src/main.rs
@@ -25,17 +25,21 @@ fn main() {
 
 
 fn response_to_client(mut stream: TcpStream){
-    let response_200 = "HTTP/1.1 200 OK\r\n\r\n";
+    let response_200 = "HTTP/1.1 200 OK";
     let response_400 = "HTTP/1.1 404 NOT FOUND\r\n\r\n";
 
     let buf_reader = BufReader::new(&mut stream);
     //let start_line = buf_reader.lines().next().expect("Request not found").expect("There was an error on the request");
     let start_line = buf_reader.lines().next().expect("Request not found").expect("There was an error on the request");
 
-    let path = start_line.split(" ").find(|&p| p == "/");
+    // let path = start_line.split(" ").find(|&p| p == "/"); Answer of respond to with 404
+    let path = start_line.split(" ").find(|&p| p.contains("/echo/"));
     match path {
-        Some(_p) => {
-            let _ = stream.write_all(response_200.as_bytes()).expect("Error while responding to client");
+        Some(p) => {
+            let random_string_from_client = p.split("/").nth(2).expect("Could not split the request header");
+            let length = random_string_from_client.len();
+            let my_response = format!( "{response_200}\r\nContent-Type: text/plain\r\nContent-Length: {length}\r\n\r\n{random_string_from_client}");
+            let _ = stream.write_all( my_response.as_bytes()).expect("Error while responding to client");
         }
         None => {
             let _ = stream.write_all(response_400.as_bytes()).expect("Error while responding to client");
