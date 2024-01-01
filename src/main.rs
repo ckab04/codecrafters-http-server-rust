de992f7ceb6bb357ef6f7747615e7ef9abb5befd
ckab04 <ciceronkapenabm@gmail.com>
2024-01-01 15:29:45 +0100

    Get a file(test 2)

--- src/main.rs
+++ src/main.rs
@@ -8,7 +8,7 @@ fn main() {
     // You can use print statements as follows for debugging, they'll be visible when running tests.
     println!("Logs from your program will appear here!");
     // Uncomment this block to pass the first stage
-    let listener = TcpListener::bind("127.0.0.1:4222").unwrap();
+    let listener = TcpListener::bind("127.0.0.1:4221").unwrap();
     for stream in listener.incoming() {
         match stream {
             Ok(stream) => {
