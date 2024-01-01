885f780dfb0e9c85d6e73f5f1bc819fe8b672b63
ckab04 <ciceronkapenabm@gmail.com>
2024-01-01 20:55:01 +0100

    Post a file (test 1)

--- src/main.rs
+++ src/main.rs
@@ -24,7 +24,7 @@ fn main() {
 }
 
 
-fn response_to_client(mut stream: TcpStream){
+fn response_to_client(mut stream: TcpStream) {
     let response_200 = "HTTP/1.1 200 OK";
     let response_400 = "HTTP/1.1 404 NOT FOUND\r\n\r\n";
 
@@ -38,31 +38,38 @@ fn response_to_client(mut stream: TcpStream){
     let start_line = http_request.get(0).expect("Error while getting the start line");
     println!("Start Line  : {:?}", start_line);
     // let path = start_line.split(" ").find(|&p| p == "/"); Answer of respond to with 404
+    let p: Vec<&str> = start_line.split(" ").collect();
+    //let path = String::new();
+    if p[0] == "GET" {
     let path = start_line.split(" ").find(|&p| p.contains("/"));
     println!("Path : {:?}", path);
     match path {
         Some(p) => {
-            if p == "/"{
+            if p == "/" {
                 let _root_path = start_line.split(" ").find(|&p| p == "/").unwrap();
-                let my_response = format!( "{response_200}\r\n\r\n");
+                let my_response = format!("{response_200}\r\n\r\n");
                 //let _ = stream.write_all( my_response.as_bytes()).expect("Error while responding to client");
                 //thread::sleep(Duration::from_secs(30));
                 write_response_to_client(&mut stream, my_response);
-            }
-            else if p.starts_with("/echo/"){
+            } else if p.starts_with("/echo/") {
                 respond_with_content(&stream, p, response_200);
-            }else if p.starts_with("/user-agent"){
+            } else if p.starts_with("/user-agent") {
                 user_agent(&stream, &http_request, response_200);
-            }else if p.starts_with("/files/"){
-                get_a_file(&stream,&start_line, response_200);
-            }
-            else{
+            } else if p.starts_with("/files/") {
+                get_a_file(&stream, &start_line, response_200);
+            } else {
                 write_response_to_client(&mut stream, response_400.to_string());
             }
         }
         None => {
             write_response_to_client(&mut stream, response_400.to_string());
         }
+    }
+    }
+    else if p[0] == "POST"{
+        //let path = start_line.split(" ").nth(1).unwrap();
+        //let filename = &path[7..];
+        post_a_file(&mut stream, &http_request);
     }
 }
 
@@ -95,7 +102,7 @@ fn get_a_file(mut stream: &TcpStream,start_line: &String, response_status: &str)
 
     let cmd_args: Vec<String> = env::args().collect();
     let directory_path = &cmd_args[2];
-    let mut dir = fs::read_dir(directory_path).expect("Could not read the directory");
+    let dir = fs::read_dir(directory_path).expect("Could not read the directory");
     let fname = &start_line[11..];
     let filename = fname.split(" ").nth(0).expect("Could not split the file name");
     println!("File name : {filename}");
@@ -109,7 +116,6 @@ fn get_a_file(mut stream: &TcpStream,start_line: &String, response_status: &str)
         let content = fs::read_to_string(dir_path).expect("Could not read the file");
         let length = content.len();
         let my_response = format!( "{response_status}\r\nContent-Type:application/octet-stream\r\nContent-Length:{length}\r\n\r\n{content}");
-        //let my_response = format!( "{response_status}\r\nContent-Type:application/octet-stream\r\n\r\n{content}");
         write_response_to_client(&mut stream,my_response);
     }
     else{
@@ -117,12 +123,23 @@ fn get_a_file(mut stream: &TcpStream,start_line: &String, response_status: &str)
         let my_response = format!( "{response_400}\r\n\r\n");
         write_response_to_client(&mut stream,my_response);
     }
-    // for entry in dir{
-    //     let entry = entry.unwrap();
-    //     if entry.file_name().to_str().unwrap().contains(filename){
-    //         let my_response = format!( "{response_status}\r\nContent-Type:application/octet-stream\r\nContent-Length:{length}\r\n\r\n{random_string}");
-    //
-    //     }
-    // }
 
+}
+
+fn post_a_file(mut stream: &TcpStream, http_request: &Vec<String>){
+    let cmd_args: Vec<String> = env::args().collect();
+    let directory_path = &cmd_args[2];
+    let start_line = &http_request[0];
+
+    let fname = &start_line[12..];
+    let filename = fname.split(" ").nth(0).expect("Could not split the file name");
+    println!("File name for POST REQUEST : {filename}");
+    let dir_path = Path::new(directory_path).join(filename);
+
+    let content = &http_request[6];
+
+    fs::write(dir_path, content.as_bytes()).expect("Could not write to file");
+    let response = "HTTP/1.1 201\r\n\r\n";
+    let my_response = format!( "{response}\r\n\r\n");
+    write_response_to_client(&mut stream,my_response);
 }
